83a4151184b4d6ca2c716e7732786d60ea44aa77
Scott Baron <rtbd11@gmail.com>
2016-12-26 05:40:19 -0500

    Built the server file to reflect the recommended

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ var dashboard = require('./routes/breweryManagement');
 var account = require('./routes/accountManagement');
 
 var app = express();
+var debug = require('debug')('ondraftapps:server');
+var http = require('http');
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -70,5 +74,62 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
 
 module.exports = app;
