19b53fdfb1861245c50b0f0d0e664d00f8e4402b
Scott Baron <baron159@live.com>
2017-01-05 00:58:37 -0500

    Brew are now saving to the database successfully.

--- routes/brewManagement.js
+++ routes/brewManagement.js
@@ -9,6 +9,8 @@ var passport = require('passport');
 var draftDB = require('../lib/onDraftDB');
 
 var rawFetchBrewQuery = 'SELECT * FROM brew_table WHERE user_id=$1::int;';
+var rawBrewIDFetchQuery = 'SELECT brewery_id FROM brewery_table WHERE user_id=$1::int LIMIT 1;';
+var rawBrewCreationQuery = 'INSERT INTO brew_table VALUES(DEFAULT, $1::int, $2::int, $3::text, $4::text, $5::text, $6::text, $7::text, $8::bool, $9::text);';
 
 router.get('/brew-page', isLoggedIn, function (req, res) {
     draftDB.draftDB.connect(function (err, client, done) {
@@ -28,7 +30,7 @@ router.get('/brew-page', isLoggedIn, function (req, res) {
                 }
                 else{
                     res.statusCode = 200;
-                    res.render('dashboardBrewView', {_csrf:req.csrfToken(), userBrews: result.rows});
+                    res.render('dashboardBrewView', {_csrf:req.csrfToken(), userBrews: result.rows, message: req.query['message']});
                 }
             })
         }
@@ -37,8 +39,62 @@ router.get('/brew-page', isLoggedIn, function (req, res) {
 
 router.post('/create-brew', isLoggedIn, function (req, res) {
     console.log(req.body);
-    res.statusCode = 300;
-    res.send({message:'Data receved'});
+    var breweryID = null;
+    var brewAvaFlag = null;
+    // We must first get the Brewery ID for the user that is using the system
+    draftDB.draftDB.connect(function (err, client, done) {
+        if(err){
+            console.error('ERROR - Unable to retrieve a Client');
+            res.statusCode = 501;
+            res.redirect('/dashboard/main?message=Brew+Service+is+unavailable+at+this+time');
+        }
+        else{
+            client.query(rawBrewIDFetchQuery, [req.user['userID']], function (err, results) {
+                if(err){
+                    console.error('ERROR - Query Error');
+                    res.statusCode = 501;
+                    res.redirect('/dashboard/main?message=Brew+Service+is+unavailable+at+this+time');
+                }
+                else{
+                    breweryID = results.rows[0]['brewery_id'];
+                    if(req.body['brewAvaNow'] == 'on'){
+                        brewAvaFlag = true;
+                    }
+                    else{
+                        brewAvaFlag = false;
+                    }
+
+                    console.log(brewAvaFlag);
+                    console.log('Brewery ID That was found: ' + breweryID);
+
+                    client.query(rawBrewCreationQuery,
+                        [req.user['userID'], breweryID, req.body['brewName'], req.body['brewStyle'], req.body['brewAbv'], req.body['brewIbu'], req.body['brewSum'], brewAvaFlag, 'NEED CHANGE'],
+                        function (err, result) {
+                        done();
+                            if(err){
+                                console.error('ERROR - Query Error');
+                                console.error(err);
+                                res.statusCode = 501;
+                                res.redirect('/dashboard/main?message=Brew+Service+is+unavailable+at+this+time');
+                            }
+                            else{
+                                console.log('Brew Created - Now redirecting to refresh the page.');
+                                res.statusCode = 300;
+                                res.redirect('/dashboard/brew-page?message=Brew+created');
+                            }
+                        });
+                }
+
+            });
+
+
+
+
+
+
+        }
+    });
+
 });
 
 function isLoggedIn(req, res, next) {
